d149e96faca86d5c8cce77ffd5f341ce87554280
Martin Průša <prusamartin.pm@gmail.com>
2023-05-15 11:21:14 +0200

    fix links width and heigth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,11 @@ export default function Home() {
           </div>
           <div className='border-2 max-w-md mx-auto border-gray-500 bg-gray-100 h-40 rounded-2xl mt-10'>
             <ul className='flex flex-col items-center w-full'>
-              <li className='bg-gray-500 hover:bg-gray-600 transition-colors rounded-2xl text-white w-1/2 text-center my-5 py-2'>
-                <Link href='/login'>Přihlásit se</Link>
+              <li className='bg-gray-500 hover:bg-gray-600 transition-colors rounded-2xl text-white w-1/2 text-center my-5 '>
+                <Link className='w-full h-full block py-2' href='/login'>Přihlásit se</Link>
               </li>
-              <li className='bg-gray-500 hover:bg-gray-600 transition-colors rounded-2xl text-white w-1/2 text-center py-2'>
-                <Link href='/register'>Registrovate se</Link>
+              <li className='bg-gray-500 hover:bg-gray-600 transition-colors rounded-2xl text-white w-1/2 text-center'>
+                <Link className='w-full h-full block py-2' href='/register'>Registrovate se</Link>
               </li>
             </ul>
           </div>

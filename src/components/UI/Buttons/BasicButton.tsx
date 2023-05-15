9f56f12029c5be19b7d0142d34ec6876f29e6ffb
Martin Průša <prusamartin.pm@gmail.com>
2023-05-15 12:23:05 +0200

    button margin fix

--- src/components/UI/Buttons/BasicButton.tsx
+++ src/components/UI/Buttons/BasicButton.tsx
@@ -5,7 +5,7 @@ export const BasicButton = ({children, action}: BasicButtonProps) => {
     return (
         <button type="button"
                 onClick={action}
-                className={`my-5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600`}>
+                className={`mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600`}>
             {children}</button>
     )
 }

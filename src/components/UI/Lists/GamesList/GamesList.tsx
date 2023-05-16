aec952d731c1b0ec892d5c6c7b7da2881b7ac999
Martin Průša <prusamartin.pm@gmail.com>
2023-05-16 19:46:46 +0200

    create games list and game card components

---
+++ src/components/UI/Lists/GamesList/GamesList.tsx
@@ -0,0 +1,11 @@
+import {useContext} from "react";
+import {AuthContext} from "@/contexts";
+
+export const GamesList = () => {
+
+    const {state, dispatch} = useContext(AuthContext)
+
+    return (
+        <div></div>
+    )
+}

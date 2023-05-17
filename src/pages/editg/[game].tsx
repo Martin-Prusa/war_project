67826ca47fd1242e0f332d30c271f04441ecb5b7
Martin Průša <prusamartin.pm@gmail.com>
2023-05-17 11:47:08 +0200

    edit game error

--- src/pages/editg/[game].tsx
+++ src/pages/editg/[game].tsx
@@ -1,7 +1,74 @@
 import {useRouter} from "next/router";
+import {NavbarComponent} from "@/components";
+import {NewGameForm} from "@/components/UI/Forms/NewGameForm";
+import {useContext, useEffect, useState} from "react";
+import {IGame} from "@/interfaces";
+import {AuthContext} from "@/contexts";
 
 export default function EditGame() {
     const router = useRouter();
     const {game} = router.query;
-    console.log(game)
+
+    const {authState, authDispatch} = useContext(AuthContext)
+
+    const [values, setValues] = useState<IGame>({
+        name: '',
+        description: '',
+        price: 0,
+        sale: 0,
+        publisher: '',
+        developer: '',
+        releaseDate: new Date(),
+        genres: []
+    })
+
+    useEffect(() => {
+        if(!authState) {
+            router.push('/')
+            return
+        }
+        fetch('http://localhost:3000/games/'+game, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authState.Authorization}`
+            }
+        }).then(res => res.json())
+            .then(data => {
+                setValues({...data, releaseDate: new Date(data.releaseDate)})
+                console.log(data)
+            })
+            .catch(e => console.log(e))
+    }, [])
+
+    const edit = () => {
+        if(!authState) {
+            router.push('/')
+            return
+        }
+        fetch('http://localhost:3000/games/'+game, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authState.Authorization}`
+            },
+            body: JSON.stringify(values)
+        }).then(res => res.json())
+            .then(data => {
+                router.push('/games')
+            })
+            .catch(e => console.log(e))
+    }
+
+    return (
+        <main>
+            <NavbarComponent />
+            <div className='container mx-auto'>
+                <div className='text-center text-4xl'>
+                    <h1 className='pt-10'>Upravit hru</h1>
+                </div>
+                <NewGameForm values={values} setValues={setValues} submitFunc={edit} submitText='Upravit hru' />
+
+            </div>
+        </main>
+    )
 }
